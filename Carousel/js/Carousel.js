4846739dc34c35ebb23d1c533fa64fc64c2b57e1
MaxWang <pro12345678902@163.com>
2018-08-02 18:10:28 +0800

    完成框架

--- Carousel/js/Carousel.js
+++ Carousel/js/Carousel.js
@@ -38,6 +38,7 @@ Carousel.prototype = {
         this.O.timer = setInterval(function(){
             console.log(that.O.num)
             that.animalImg(that)
+            that.animalBtn(that)
         },that.O.time)
         
     },
@@ -57,8 +58,14 @@ Carousel.prototype = {
     createBtn:function(){
         var btn = this._createELement('div',this.O.el,"btn")
         for(let i = 0;i < this.length;i++){
-            this._createELement('span',btn,'btn_arrow')
+            if(i == 0){
+                this._createELement('span',btn,'btn_round btn_hover')
+            }else{
+                this._createELement('span',btn,'btn_round')
+            }
         }
+        this.btn = this.O.el.querySelectorAll('.btn')[0]
+        this.rounds = this.O.el.querySelectorAll('.btn_round')
     },
     _createELement:function(el,parent,classname){
         var a = document.createElement(el)
@@ -69,15 +76,13 @@ Carousel.prototype = {
     //animal
     animalImg:function(that){
         var pLeft = that.pic.offsetLeft
-        console.log(pLeft + '  ' + (-that.O.num * that.Width))
+        //console.log(pLeft + '  ' + (-that.O.num * that.Width))
         //                                                           
         // 0. 000      1. -200      2. -400      3. -600
         // 0 * w + w   1 * w + w    2 * w + w    3 * w  + w 
-
         if(pLeft == 0 && that.O.num == that.length){
             that.O.num = 1
         }else if(that.O.num == that.length ){
-            console.log('!!')
             that.pic.style.left = pLeft + (that.Width * (that.length - 1) * 0.1) + 'px'
             that.O.times = setTimeout(function(){that.animalImg(that)},100)
         }else if(pLeft !== (-that.O.num * that.Width)){
@@ -85,6 +90,19 @@ Carousel.prototype = {
             that.O.times = setTimeout(function(){that.animalImg(that)},100)
         }else{
             that.O.num++
+        }
+    },
+    animalBtn:function(that){
+        console.log(that.O.num - 1)
+        for(let i = 0;i < that.rounds.length;i++){
+            if(that.O.num == that.length){
+                that.rounds[0].setAttribute('class','btn_round btn_hover')
+                that.rounds[that.length - 1].setAttribute('class','btn_round')
+            }else if(i == that.O.num){
+                that.rounds[i].setAttribute('class','btn_round btn_hover')
+            }else{
+                that.rounds[i].setAttribute('class','btn_round')
+            }
         }
     }
 
